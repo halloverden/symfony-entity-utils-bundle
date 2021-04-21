0ffc6ea6ebfa46f8c5f2e2b323a10d8ed3a493ef
tomme87 <tom.roar@halloverden.no>
2021-04-21 17:47:50 +0200

    FIX: missing phpdoc

--- Interfaces/GenericEntityInterface.php
+++ Interfaces/GenericEntityInterface.php
@@ -3,6 +3,10 @@
 
 namespace HalloVerden\EntityUtilsBundle\Interfaces;
 
-
+/**
+ * Interface GenericEntityInterface
+ *
+ * @package HalloVerden\EntityUtilsBundle\Interfaces
+ */
 interface GenericEntityInterface extends TimestampableEntityInterface, NonPrimaryKeyUuidPropertyInterface, ValidatableEntityInterface {
 }
