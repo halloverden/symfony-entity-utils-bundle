5973794a07391d2223b779b6ae68d9022e4a3ec4
tomme87 <tom.roar@halloverden.no>
2021-04-21 17:47:32 +0200

    EDIT: use ConfigurableExtension instead

--- DependencyInjection/HalloVerdenEntityUtilsExtension.php
+++ DependencyInjection/HalloVerdenEntityUtilsExtension.php
@@ -7,25 +7,26 @@ namespace HalloVerden\EntityUtilsBundle\DependencyInjection;
 use HalloVerden\EntityUtilsBundle\EventListener\EntityValidatorListener;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\HttpKernel\DependencyInjection\ConfigurableExtension;
 
-class HalloVerdenEntityUtilsExtension extends Extension {
+/**
+ * Class HalloVerdenEntityUtilsExtension
+ *
+ * @package HalloVerden\EntityUtilsBundle\DependencyInjection
+ */
+class HalloVerdenEntityUtilsExtension extends ConfigurableExtension {
 
   /**
    * @inheritDoc
-   * @throws \Exception
    */
-  public function load(array $configs, ContainerBuilder $container) {
-    $configuration = new Configuration();
-    $config = $this->processConfiguration($configuration, $configs);
-
+  protected function loadInternal(array $mergedConfig, ContainerBuilder $container) {
     $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
     $loader->load('services.yaml');
 
-    if (isset($config['entity_validator']['validation_groups'])) {
+    if (isset($mergedConfig['entity_validator']['validation_groups'])) {
       $entityValidatorListener = $container->getDefinition(EntityValidatorListener::class);
-      $entityValidatorListener->setArgument('$validationGroups', $config['entity_validator']['validation_groups']);
+      $entityValidatorListener->setArgument('$validationGroups', $mergedConfig['entity_validator']['validation_groups']);
     }
   }
 
