66ccc1d08c4a4a8d2202a521e2b7f3b04125668f
tomme87 <tom.roar@halloverden.no>
2021-04-21 17:47:56 +0200

    FIX: missing phpdoc

--- Interfaces/NonPrimaryKeyUuidPropertyInterface.php
+++ Interfaces/NonPrimaryKeyUuidPropertyInterface.php
@@ -6,7 +6,21 @@ namespace HalloVerden\EntityUtilsBundle\Interfaces;
 
 use Symfony\Component\Uid\Uuid;
 
+/**
+ * Interface NonPrimaryKeyUuidPropertyInterface
+ *
+ * @package HalloVerden\EntityUtilsBundle\Interfaces
+ */
 interface NonPrimaryKeyUuidPropertyInterface {
+
+  /**
+   * Generates Uuuid
+   */
   function generateUuid(): void;
+
+  /**
+   * @return Uuid|null
+   */
   function getUuid(): ?Uuid;
+
 }
