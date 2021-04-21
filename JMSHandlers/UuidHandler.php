a9e934e2d6029f3f5f817c3ba754a0d91e07baaf
tomme87 <tom.roar@halloverden.no>
2021-04-21 17:50:13 +0200

    ADD: UuidHandler

---
+++ JMSHandlers/UuidHandler.php
@@ -0,0 +1,65 @@
+<?php
+
+
+namespace HalloVerden\EntityUtilsBundle\JMSHandlers;
+
+
+use JMS\Serializer\Context;
+use JMS\Serializer\GraphNavigatorInterface;
+use JMS\Serializer\Handler\SubscribingHandlerInterface;
+use JMS\Serializer\JsonDeserializationVisitor;
+use JMS\Serializer\JsonSerializationVisitor;
+use Symfony\Component\Uid\Uuid;
+
+/**
+ * Class UuidHandler
+ *
+ * @package HalloVerden\EntityUtilsBundle\JMSHandlers
+ */
+class UuidHandler implements SubscribingHandlerInterface {
+
+  /**
+   * @inheritDoc
+   */
+  public static function getSubscribingMethods(): array {
+    return [
+      [
+        'direction' => GraphNavigatorInterface::DIRECTION_SERIALIZATION,
+        'format' => 'json',
+        'type' => 'Uuid',
+        'method' => 'serializeUuid'
+      ],
+      [
+        'direction' => GraphNavigatorInterface::DIRECTION_DESERIALIZATION,
+        'format' => 'json',
+        'type' => 'Uuid',
+        'method' => 'deserializeUuid'
+      ]
+    ];
+  }
+
+  /**
+   * @param JsonSerializationVisitor $visitor
+   * @param Uuid                     $uuid
+   * @param array                    $type
+   * @param Context                  $context
+   *
+   * @return string
+   */
+  public function serializeUuid(JsonSerializationVisitor $visitor, Uuid $uuid, array $type, Context $context): string {
+    return $uuid->jsonSerialize();
+  }
+
+  /**
+   * @param JsonDeserializationVisitor $visitor
+   * @param string                     $uuidAsString
+   * @param array                      $type
+   * @param Context                    $context
+   *
+   * @return Uuid
+   */
+  public function deserializeUuid(JsonDeserializationVisitor $visitor, string $uuidAsString, array $type, Context $context): Uuid {
+    return Uuid::fromString($uuidAsString);
+  }
+
+}
